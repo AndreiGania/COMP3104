d95e5fecc145c97f9f7d9e5e397ea0ead975ee5f
Iznmav <andreijhosephgania@gmail.com>
2025-10-04 18:15:29 -0400

    react app build

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: StudentID || Student Name || DevOps</h5>
+      <h5>Developed By: 101478350 || Andrei Gania || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
