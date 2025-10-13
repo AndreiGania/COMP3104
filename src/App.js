b6a3b816794a2bfcaf29cf3f79224ec989015451
Iznmav <andreijhosephgania@gmail.com>
2025-10-12 20:45:58 -0400

    Name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101478350 || Andrei Gania || DevOps</h5>
+      <h5>Developed By: 101478350 || Andrei Gania || COMP3104 DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
